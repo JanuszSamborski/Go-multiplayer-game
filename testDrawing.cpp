c088961988f5ba3ca78697d5c5ec331541487f5b
JanuszSamborski <50918905+JanuszSamborski@users.noreply.github.com>
2019-06-24 16:58:29 +0200

    Add files via upload

--- testDrawing.cpp
+++ testDrawing.cpp
@@ -9,6 +9,10 @@ int main()
 {
 	logic game;
 	game.initializeBoard(game.board, 9, game.captured_white, game.captured_black);
+	int a[]={0,0};
+	int b[]={0,1};
+	game.addStone(game.board, game.BLACK, a, game.captured_black, game.captured_white);
+	game.addStone(game.board, game.WHITE, b, game.captured_black, game.captured_white);
 	game.drawBoard(game.board);
 	return 0;
 }
