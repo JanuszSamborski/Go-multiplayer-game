2e60cb0ca849a2dcd33e73f69105b40d69af04d8
ConsoleRunner <wojciechly@wp.pl>
2019-06-25 15:06:05 -0400

    added: client and server test files

---
+++ testCli.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"networking.h"
+
+using namespace std;
+
+int main()
+{
+	networkingClient Client;
+
+	
+
+	return 0;
+}
---
+++ testSrv.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"networking.h"
+
+using namespace std;
+
+int main()
+{
+	networkingServer Client;
+
+	
+
+	return 0;
+}
