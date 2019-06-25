e1b5e7e69fa8829b5e43144c61b3135b55e1d072
JanuszSamborski <janusz.joe.samborski@gmail.com>
2019-06-25 13:39:46 +0200

    extended test

--- testDrawing.cpp
+++ testDrawing.cpp
@@ -12,10 +12,9 @@ int main()
 	game.board_size = 19;
 	game.local_player_color = game.BLACK;
 	game.initializeBoard(game.board, game.board_size, game.captured_white, game.captured_black);
-	int a[]={0,0};
-	int b[]={0,1};
-	game.addStone(game.board, game.BLACK, a, game.captured_black, game.captured_white);
-	game.addStone(game.board, game.WHITE, b, game.captured_black, game.captured_white);
+	int b[8][2]={{1,1},{1,2},{1,3},{2,1},{2,3},{3,1},{3,2},{3,3}};
+	for(int i=0; i<8; i++)
+		game.addStone(game.board, game.WHITE, b[i], game.captured_black, game.captured_white);
 	game.drawBoard(game.board, make_pair(0,0), game.cursor_player, game.captured_black, game.captured_white);
 	while(true)
 	{
