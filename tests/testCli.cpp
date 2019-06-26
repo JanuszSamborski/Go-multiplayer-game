af699a1bd44984495ef3ea7636914471f6064137
ConsoleRunner <wojciechly@wp.pl>
2019-06-26 19:20:07 -0400

    added: tests folder, moved tests accordingly

---
+++ tests/testCli.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include"networking.h"
+#include"goGame.h"
+
+using namespace std;
+
+void sendReceiveTest()
+{
+	networkingClient Client((char*)"2001:db8:0:f101::1");
+
+	tlv test, test2;
+
+	test.type 	 = 0x1;
+	test.length  = 2;
+	test.value.x = 10;
+	test.value.y = 20;
+
+	Client.sendMessage(test);
+
+	test2 = Client.receiveMessage();
+
+	cout<<"type: " + to_string(test2.type)<<endl
+		<<"length: " + to_string(test2.length)<<endl
+		<<"value 1: " + to_string(test2.value.x)<<endl
+		<<"value 2: " + to_string(test2.value.y)<<endl;
+
+	test2 = Client.receiveMessage();
+
+	cout<<"type: " + to_string(test2.type)<<endl
+		<<"length: " + to_string(test2.length)<<endl
+		<<"value 1: " + to_string(test2.value.x)<<endl
+		<<"value 2: " + to_string(test2.value.y)<<endl;
+}
+
+int main()
+{
+	networkingClient Client((char*)"2001:db8:0:f101::1");
+	goGame Game(&Client);
+
+	Game.initializeBoard();
+
+	Game.run();
+
+	return 0;
+}
---
+++ tests/testSrv.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include"networking.h"
+#include"goGame.h"
+
+using namespace std;
+
+void sendReceiveTest()
+{
+	networkingServer Server;
+
+	tlv test,test2;
+
+	test = Server.receiveMessage();
+
+	cout<<"type: " + to_string(test.type)<<endl
+		<<"length: " + to_string(test.length)<<endl
+		<<"value 1: " + to_string(test.value.x)<<endl
+		<<"value 2: " + to_string(test.value.y)<<endl;
+
+	test2.type	  = 0x1;
+	test2.length  = 2;
+	test2.value.x = 10;
+	test2.value.y = 30;
+
+	Server.sendMessage(test2);
+
+	test2.type	  = 0x5;
+	test2.length  = 2;
+	test2.value.x = 12;
+	test2.value.y = 13;
+
+	Server.sendMessage(test2);
+}
+
+int main()
+{
+	networkingServer Server;
+	goGame Game(&Server);
+
+	Game.setBoardSize(19);
+	Game.initializeBoard();
+
+	Game.run();
+
+	return 0;
+}
